3abbbf2134a9ea64d274b0d8aac0d3d6b770d0b8
Luke Smyth <smythluke123@gmail.com>
2016-04-23 00:16:30 +0100

    Initial Commit
    First commit. Working smart mirror web app.
    contains time, date, current weather, sunrise, sunset, BBC news feed.
    Will speak the weather at 6:35 on weekdays.

---
+++ info.py
@@ -0,0 +1,127 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from threading import Thread
+import urllib.request
+import feedparser
+import json
+import time
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+smallNewsJSON = {}
+smallWeatherJSON = {}
+
+icons = {
+	'day': {
+		'clear': 'wi-day-sunny',
+		'cloudy': 'wi-day-cloudy',
+		'flurries': 'wi-snow',
+		'fog': 'wi-fog',
+		'hazy': 'wi-day-haze',
+		'mostlycloudy': 'wi-day-cloudy',
+		'mostlysunny': 'wi-day-cloudy',
+		'partlycloudy': 'wi-day-cloudy',
+		'partlysunny': 'wi-day-cloudy',
+		'sleet': 'wi-sleet',
+		'rain': 'wi-rain',
+		'snow': 'wi-snow',
+		'sunny': 'wi-day-sunny',
+		'tstorms': 'wi-day-thunderstorm'
+	}, 
+	'night': {
+		'clear': 'wi-night-clear',
+		'cloudy': 'wi-night-alt-cloudy',
+		'flurries': 'wi-night-alt-snow',
+		'fog': 'wi-night-fog',
+		'hazy': 'wi-night-fog',
+		'mostlycloudy': 'wi-night-alt-cloudy',
+		'mostlysunny': 'wi-night-alt-cloudy',
+		'partlycloudy': 'wi-night-alt-cloudy',
+		'partlysunny': 'wi-night-alt-cloudy',
+		'sleet': 'wi-night-alt-sleet',
+		'rain': 'wi-night-alt-rain',
+		'snow': 'wi-night-alt-snow',
+		'sunny': 'wi-night-clear',
+		'tstorms': 'wi-night-alt-thunderstorm'
+	}
+}
+
+@socketio.on('connect')
+def connected():
+	global newsThreadStarted
+	global weatherThreadStarted
+	
+	if not newsThreadStarted:
+		newsThread.start()
+		newsThreadStarted = True
+	else:
+		socketio.emit('weather', smallWeatherJSON)
+	
+	if not weatherThreadStarted:
+		weatherThread.start()
+		weatherThreadStarted = True
+	else:
+		socketio.emit('news', smallNewsJSON)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+	
+def newsThread():
+	while True:
+		newsFeed = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml?edition=uk")
+		#newsFeed = feedparser.parse(r'bbc.xml')
+		for i, entry in enumerate(newsFeed.entries):
+			smallNewsJSON[i] = {
+				'summary' : entry.summary
+			}
+			if i == 30:
+				break
+		
+		socketio.emit('news', smallNewsJSON)
+		time.sleep(600)
+
+def weatherThread():
+	while True:
+		print("making API call to wunderground")
+		resource = urllib.request.urlopen("http://api.wunderground.com/api/" + API_KEY + "/astronomy/conditions/forecast/q/UK/Cheltenham.json")
+		content =  resource.read().decode(resource.headers.get_content_charset())
+		weatherJSON = json.loads(content)
+		#with open('weather.json') as weather:
+		#	weatherJSON = json.load(weather)
+		global smallWeatherJSON
+		global icons
+		smallWeatherJSON['temperature'] = weatherJSON['current_observation']['temp_c']
+		smallWeatherJSON['temperatureHigh'] = weatherJSON['forecast']['simpleforecast']['forecastday'][0]['high']['celsius']
+		smallWeatherJSON['temperatureLow'] = weatherJSON['forecast']['simpleforecast']['forecastday'][0]['low']['celsius']
+		smallWeatherJSON['sunrise'] = "%02d" % int(weatherJSON['sun_phase']['sunrise']['hour']) + ':' + "%02d" % int(weatherJSON['sun_phase']['sunrise']['minute'])
+		smallWeatherJSON['sunset'] = "%02d" % int(weatherJSON['sun_phase']['sunset']['hour']) + ':' + "%02d" % int(weatherJSON['sun_phase']['sunset']['minute'])
+		smallWeatherJSON['forecastString'] = weatherJSON['forecast']['txt_forecast']['forecastday'][0]['fcttext_metric']
+		smallWeatherJSON['currentWeather'] = weatherJSON['current_observation']['weather']
+		
+		currentTime = "%02d" % int(datetime.now().hour) + ':' + "%02d" % int(datetime.now().minute)
+		currentIcon = weatherJSON['current_observation']['icon']
+		
+		if currentTime > smallWeatherJSON['sunrise'] and currentTime < smallWeatherJSON['sunset']:
+			smallWeatherJSON['weatherIcon'] = icons['day'][currentIcon]
+		else :
+			smallWeatherJSON['weatherIcon'] = icons['night'][currentIcon]
+		
+		
+		socketio.emit('weather', smallWeatherJSON)
+		time.sleep(1800)
+		
+newsThread = Thread(target=newsThread)
+newsThread.daemon = True
+newsThreadStarted = False
+
+weatherThread = Thread(target=weatherThread)
+weatherThread.daemon = True
+weatherThreadStarted = False
+	
+if __name__ == "__main__":
+    socketio.run(app)
---
+++ static/js/info.js
@@ -0,0 +1,98 @@
+window.onload = function() {
+	
+	startTime();
+	
+	socket = io.connect('http://' + document.domain + ':' + location.port + '/');	
+	socket.on('weather', function(json){
+		weather = json;
+		$('#temperature').html(json.temperature + "&deg;C");
+		$('#temperatureHigh').html('<i class="wi wi-direction-up"></i> ' + json.temperatureHigh + "&deg;C");
+		$('#temperatureLow').html('<i class="wi wi-direction-down"></i> ' + json.temperatureLow + "&deg;C");
+		$('#weatherIcon').html('<i class="wi ' + json.weatherIcon + '"></i>');
+		$('#sunrise').html('<i class="wi wi-sunrise"></i> ' + json.sunrise);
+		$('#sunset').html('<i class="wi wi-sunset"></i> ' + json.sunset);
+	});
+	
+	socket.on('news', function(json){
+		news = json;
+		smallNewsJSON = json;
+		rollNews();
+	});
+		
+};
+var weather = {};
+var news = {};
+var days=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novermber', 'December'];
+
+function startTime() {
+	var today = new Date();
+	var h = checkTime(today.getHours());
+	var m = checkTime(today.getMinutes());
+	var s = checkTime(today.getSeconds());
+	$('#time').html(h + ":" + m);
+	$('#date').html(days[today.getDay()] + ", " + ordinal_suffix_of(today.getDate()) + " " + months[today.getMonth()])
+	if(today.getDay() > 0 && today.getDay() < 6){
+		if(($('#time').html()+":"+s) == "06:35:00"){
+			if(!window.speechSynthesis.speaking){
+				speak("Good-morning. The forecast for today is " + weather.forecastString + ". The current weather is " + weather.currentWeather);
+			}
+		}
+	}
+	var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+var currentNewsItem = 0;
+
+function rollNews(){
+	$("#news").html(smallNewsJSON[currentNewsItem].summary);
+	currentNewsItem = (currentNewsItem + 1) % (Object.keys(smallNewsJSON).length)
+	var x = setTimeout(rollNews, 20000);
+}
+
+function ordinal_suffix_of(i) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "<sup>st</sup>";
+    }
+    if (j == 2 && k != 12) {
+        return i + "<sup>nd</sup>";
+    }
+    if (j == 3 && k != 13) {
+        return i + "<sup>rd</sup>";
+    }
+    return i + "<sup>th</sup>";
+}
+
+function speak(text){
+	if(window.speechSynthesis.speaking){
+		window.speechSynthesis.pause();
+		window.speechSynthesis.cancel();
+	}
+	var msg = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(msg);
+	window.speechSynthesis.resume();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
